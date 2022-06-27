cde32c2720f2b826e86fb3edd138d2a996dfa34c
Samuel Ezedi <sammy.ezedi@gmail.com>
2022-06-27 06:51:10 +0100

    added database, create user, create user session, create note, delete note

--- website/views.py
+++ website/views.py
@@ -1,7 +1,33 @@
-from flask import Blueprint
+from nis import cat
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
+@login_required
 def home():
-    return "<h1>test</h1>"
+    if request.method == 'POST':
+        note = request.form.get('note')
+        if len(note) == 0:
+            flash('no text written', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added successfully!', category='success')
+    return render_template("home.html", user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = data['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+            return jsonify({})
